2f4b721a6cb1a728829b25af405580e3e7949c14
Muhammad Qasim <emmque512@gmail.com>
2021-11-16 14:39:33 +0500

    Update HomeController.js

--- src/components/HomeController.js
+++ src/components/HomeController.js
@@ -42,6 +42,7 @@ class Home extends React.Component{
                                         
                                     </ul>
                                 </nav>
+                                <a href="#" class="button button-primary">Connect Wallet</a>
                             </div>
                         </div>
                     </div>
@@ -55,10 +56,17 @@ class Home extends React.Component{
                                 <div className="btn-wrap">
                                     <a href="#" className="button button-ex-large">Mint</a>
                                 </div>
+                                <div class="inc-dec-form">
+                                    <form autocomplete="off">
+                                    <div class="value-button" id="decrease" value="Decrease Value">-</div>
+                                    <input type="number" id="number" value="0" />
+                                    <div class="value-button" id="increase" value="Increase Value">+</div>
+                                    </form>
+						        </div>
                             </div>
                             <div className="social-links">
-                                <a href="#"><img src="/images/icons/twitter.webp" alt="twitter-icon" /></a>
-                                <a href="#"><img src="/images/icons/discord.webp" alt="discrod-icon" /></a>
+                                <a href="https://twitter.com/mighty_meerkats"><img src="/images/icons/twitter.webp" alt="twitter-icon" /></a>
+                                <a href="https://discord.com/invite/mightymeerkats"><img src="/images/icons/discord.webp" alt="discrod-icon" /></a>
                             </div>
                         </div>
                         <div className="info-bar">
@@ -99,6 +107,7 @@ class Home extends React.Component{
                                         Stage 1
                                     </h3>
                                     <ul>
+                                        <li>Buy a Genesis Kong during mint!</li>
                                         <li>Sell out of primary collection.</li>
                                         <li>Begin blue-chip purchase war path - Genesis Kong, CryptoPunk, Fidenza, Cool Cats &amp; many more as voted by the DAO.</li>
                                         <li>Rally the Meerkat army - purchase large community projects voted on by the Meerkat Community.</li>
@@ -110,24 +119,18 @@ class Home extends React.Component{
                                         Stage 2
                                     </h3>
                                     <ul>
-                                        <li>Launch Pad Accelerator implemented with token burn utility.</li>
+                                        <li>Fastest ever Launch Pad Accelerator implemented with token burn utility.</li>
                                         <li>Alpha Jungle Channel access for all holders.</li>
-                                        <li>Merchandise store created.</li>
+                                        <li>24/h NFT data request channel.</li>
+                                        <li>Merchandise store created with token utility. </li>
                                         <li>Burrows &amp; Venom released for mutation or breeding that require token burning.</li>
-                                        <li>What will evolve? What rewards await? All will be revealed soon.</li>
+                                        <li>Meerkat University - special access to online classes with respected experts in the NFT &amp; Crypto space. Open to non-holders who use $GRUBS. Also use $GRUBS for VIP classes/events.</li>
                                     </ul>
                                 </div>
                                 <div className="stage stage3">
                                     <h3 className="stage__title aos-init" data-aos="fade-out-custom" data-aos-easing="new-easing">
-                                        Stage 3
+                                        Stage 3 coming soon
                                     </h3>
-                                    <ul>
-                                        <li>VX avatars for Mighty Meerkats.</li>
-                                        <li>Sandbox Purchase.</li>
-                                        <li>Meerkat Manor is created.</li>
-                                        <li>Treasure hunt launched. </li>
-                                        <li>Next three stages announced.</li>
-                                    </ul>
                                 </div>
                             </div>
                         </div>
@@ -141,6 +144,16 @@ class Home extends React.Component{
                                         <h3 className="team-member__title">Founder</h3>
                                         <img src="/images/founder.png" alt="founder image"/>
                                         <a href="#" className="team-member__url">@NFTEINSTEIN</a>
+                                        <p>Hi everyone! I am the proud founder of Mighty Meerkats! You may know me as NFTEINSTEIN on Twitter or Mr Pickle from discord. The first question I often get asked is why do this project? My response is simple, why not! I have never in my life felt the power of community as strong as I have in the NFT space. What is happening in this space will go down in history as one of the biggest cultural movements. It would be my privilege to build something that gives back to the community that has given me so much. </p>
+                                        <p>Some background on me:</p>
+                                        <ul>
+    <li>I live in Sydney, Australia - Aussie! Aussie! Aussie! </li>
+    <li>I am an admitted solicitor.   </li>
+    <li>I was the founder of one of the biggest tech platform launches in Australia with millions of dollars of free advertising generated from my marketing plan.</li>
+    <li>I have extensive experience in building e-commerce businesses.
+    </li>
+    <li>I love building brands as much as I love NFTS!</li>
+</ul>s
                                     </div>
                                 </div>
                                 <div className="team-member">
@@ -148,22 +161,25 @@ class Home extends React.Component{
                                         <h3 className="team-member__title">Developer</h3>
                                         <img src="../images/developer.png" alt="Developer image" />
                                         <a href="#" className="team-member__url">THE WIZARD</a>
+                                        <p>A veteran developer! What the wizard can do with code is truly magical. The Wizard has a Masters of Computer Science and 4 years of experience in Solidity. The Wizard works professionally in DeFi where staking contracts holding millions of dollars have been deployed.</p>
                                     </div>
                                 </div>
                             </div>
                             <div className="row">
                                 <div className="team-member">
                                     <div className="team-member__inner">
-                                        <h3 className="team-member__title">Head Mod</h3>
-                                        <img src="/images/head-mod.png" alt="Head Mod image" />
-                                        <a href="#" className="team-member__url">BZ#5932</a>
+                                        <h3 className="team-member__title">The Artist</h3>
+                                        <img src="/images/artist.png" alt="Artist image"/>
+                                        <a href="#" className="team-member__url">Leonardo da Meerkat</a>
+                                        <p>A veteran developer! What the wizard can do with code is truly magical. The Wizard has a Masters of Computer Science and 4 years of experience in Solidity. The Wizard works professionally in DeFi where staking contracts holding millions of dollars have been deployed.</p>
                                     </div>
                                 </div>
                                 <div className="team-member">
                                     <div className="team-member__inner">
-                                        <h3 className="team-member__title">Artist</h3>
-                                        <img src="/images/artist.png" alt="Artist image"/>
-                                        <a href="#" className="team-member__url">Leonardo da Meerkat</a>
+                                        <h3 className="team-member__title">Head Mod</h3>
+                                        <img src="/images/head-mod.png" alt="Head Mod image" />
+                                        <a href="#" className="team-member__url">BZ#5932</a>
+                                        <p>The personification of a top Aussie bloke! BZ has meerkat blood running through his veins! If you ever bump into him on discord ask him if he wants to hear a joke.</p>
                                     </div>
                                 </div>
                             </div>
@@ -195,21 +211,33 @@ class Home extends React.Component{
                                 <h3 className="faq__title">How do I contact someone for support or help?</h3>
                                 <p>Please visit our discord: <a href="https://discord.gg/mightymeerkats" target="_blank">https://discord.gg/mightymeerkats</a></p>
                             </div>
+                            <div className="faq-logo">
+                                <img src="/images/rgtrewgt.png" alt="Site Logo" />
+                                <a href="#" class="button button-primary">Connect Wallet</a>
+                            </div>
+                        
+
                         </div>
                     </section>
 		        </div>
                 <footer id="footer">
                     <div class="inner">
                         <div class="row">
+                        <p class="disclamier">DISCLAIMER:</p>
+                                <p class="footertext">
+                                    Mighty Meerkats and the utility token $GRUBS is not an investment and has no economic value. Mighty Meerkat NFTs are exclusively and limited to being Non-Fungible Tokens for users to collect. There is no guarantee or promise that Mighty Meerkat NFTs hold any monetary value. Mighty Meerkats NFT does NOT distribute any form of royalty or dividend to its holders. Mighty Meerkats hereby grants you a worldwide, non-exclusive, personal, limited license, solely with respect to any Mighty Meerkat NFT(s) that you Own, to display the Artwork associated with such Mighty Meerkat NFT(s), privately or publicly, solely for personal, non-commercial purposes, including on social media platforms, digital galleries, or otherwise on the Internet or in association with your offer to sell or trade your Mighty Meerkat NFT(s). This license does not grant you any rights in or to the Artwork separate from the associated Mighty Meerkats NFT(s), only limited personal non-commercial use and resale rights in the NFT are granted and you have no right to license, commercially exploit, reproduce, distribute, prepare derivative works, or publicly perform. All copyright and other rights are reserved and not granted. You agree to waive any class action status and any legal dispute around the Mighty Meerkats NFT. You agree not to hold Mighty Meerkats NFT liable for any losses; including but not limited to any gas fees for failed transactions or any excessive gas fees during minting. You agree that Mighty Meerkats NFT will not be liable for any inability for you to access the Artwork for any reason, including as a result of any downtime, failure, obsolescence, removal, termination, or other disruption relating to (a) the servers upon which the Artwork is stored; (b) any Third-Party Vendor; or (c) any other NFT platform. You agree that you are over 18 years of age or the legal age required within your local jurisdiction. Your rights and remedies in the event of any legal action against Mighty Meerkats NFT or its creators are strictly limited to the right, if any, to recover damages in an action at law, and you acknowledge that your remedy of money damages is adequate. You will not be entitled by reason of any such breach, and you will not seek, any equitable relief, whether injunctive or otherwise. In the event of any legal dispute arising to Mighty Meerkats NFT you agree to only bring the case to a binding arbitration. You agree to be bound solely by the jurisdiction of the state of New South Wales, Australia. You agree that any legal proceedings you initiate can only be brought forward in  the jurisdiction of the state of New South Wales, Australia.
+                                </p>
+
+                        </div>
+                        <div class="row">
                             <div></div>
                             <div class="footer-info">
-                                <p>DISCLAIMER:</p>
                                 <p class="coptyright">© 2021 Mighty Meerkats</p>
                             </div>
                             <div class="social">
-                                <a href="#" class="social-link"><img src="/images/icons/twitter-light.webp" alt="twitter icon"/></a>
-                                <a href="#" class="social-link"><img src="/images/icons/discord-light.webp" alt="discord icon"/></a>
-                                <a href="#" class="social-link"><img src="/images/icons/instagram-light.webp" alt="instagram icon"/></a>
+                                <a href="https://twitter.com/mighty_meerkats" class="social-link"><img src="/images/icons/twitter-light.webp" alt="twitter icon"/></a>
+                                <a href="https://discord.com/invite/mightymeerkats" class="social-link"><img src="/images/icons/discord-light.webp" alt="discord icon"/></a>
+                                <a href="https://www.instagram.com/MIGHTYMEERKATS/" class="social-link"><img src="/images/icons/instagram-light.webp" alt="instagram icon"/></a>
                             </div>
                         </div>
                     </div>
